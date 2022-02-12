45108b32ea076f36f6f2ee2b1c1b2d807a4bf2b8
kevinkimcd <kevinkimcd@gmail.com>
2022-02-12 15:16:11 -0500

    removed duplicate migration file, created first seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,97 @@
 #
 # Examples:
 #
+Project.create(
+  title: "Customer service chatbot assistant",
+  client_name: "US-based cable & Internet provider",
+  blurb: "Product planning and design of chatbot assistant to transition common phone-based customer support inquiries to web-based chat and SMS",
+  description: "Client had a chatbot assistant to help customers with basic customer service needs. For their next major iteration, the goal was to enhance the assistant with the ability to handle more complex inquiries and directly interface with internal systems.
+
+  We identified the customer support categories that normally require the most agent involvement, as these represent the highest cost from a care channel standpoint. We designed conversation flows for this initial set of support categories and drafted technical requirements to support the full journey from the backend.",
+  company: "Accenture Strategy",
+  start_year: 2020,
+  end_year: 2021,
+)
+
+Project.create(
+  title: "Video streaming platform",
+  client_name: "Media & entertainment conglomerate",
+  blurb: "Workstream lead for development and launch of new video streaming service",
+  description: "Client was developing a new video streaming service for an initial launch in the US market. In addition to broader program management and data delivery management responsibilities, we led individual workstreams and their deliverables.
+
+  As lead of the reporting workstream, we defined the process by which internal data sources were used to calculate revenue share, as negotiated with vendors and partners. Given that revenue share amounts were derived from performance metrics involving complex conditional logic, we designed visualizations of the end-to-end data flow process along with the calculation methodology.",
+  company: "Accenture Strategy",
+  start_year: 2019,
+  end_year: 2020,
+)
+
+Project.create(
+  title: "Customer app store & platform",
+  client_name: "Multinational broadcasting company",
+  blurb: "Product strategy support and executive presentation design for media streaming app platform",
+  description: "Client operated in multiple markets around the world, offering a diverse lineup of media streaming devices and TV set-top boxes. These devices ran on fragmented operating systems with different consumer app stores. This meant that an engineer developing a streaming app for client\'s ecosystem (e.g., a Netflix app) needed to write the same app multiple times for each app store.
+
+  In an effort to streamline the process for app developers and their own internal operations, client sought to form a strategy to unify their technology stack and build a single organization-wide app platform that could run on all of their hardware across all markets.",
+  company: "Accenture Strategy",
+  start_year: 2021,
+  end_year: 2021,
+)
+
+Project.create(
+  title: "Media streaming device",
+  client_name: "Multinational broadcasting company",
+  blurb: "Product & strategy assessment of media streaming device",
+  description: "Client launched a media streaming device several years ago in a competitive and saturated market, and has since struggled with user adoption and engagement. We helped client assess their entire end-to-end customer lifecycle strategy and took an in-depth look at the product itself. We analyzed the functionality of the product in the context of their main competitors, and made recommendations to improve the product as well as their overall sales strategy.",
+  company: "Accenture Strategy",
+  start_year: 2020,
+  end_year: 2020,
+)
+
+Project.create(
+  title: "Audience management platform",
+  client_name: "US-based broadcasting company",
+  blurb: "Product management of an audience targeting application for linear TV",
+  description: "Client was developing an internal application to build interest-based audiences that would allow advertisers to target specific cohorts of viewers on linear (live) television format. Business stakeholders provided a set of objectives that were used to build detailed product requirements, feature descriptions, epics, and user stories across a two-year roadmap.",
+  company: "Accenture Strategy",
+  start_year: 2018,
+  end_year: 2019,
+)
+
+Project.create(
+  title: "Product planning & portfolio management",
+  client_name: "US-based broadcasting company",
+  blurb: "Product support for development of an ad campaign planning application",
+  description: "Client was in the process of overhauling legacy processes in their ad tech division that relied on duplicative manual tasks maintained in spreadsheets. For the development of their new ad campaign planning tool, we defined the product requirements and use cases, along with initial wireframes and mockups.
+
+  In addition, we assessed the organization\'s existing portfolio of ad tech products as a whole, including those of recent acquisitions. We made recommendations on portfolio optimization through an analysis of each advertising product in the context of the overall advertising value chain and market landscape.",
+  company: "IBB Consulting Group",
+  start_year: 2016,
+  end_year: 2016,
+)
+
+Project.create(
+  title: "Go-to-market strategy transformation",
+  client_name: "Global technology & services company",
+  blurb: "Data analytics, client segmentation, and strategy development for organizational go-to-market transformation",
+  description: "Client was undergoing a major organizational change, which included a global go-to-market sales strategy transformation. Seeking to make optimizations in the way their sales executives are assigned to existing and prospective customers, we built a model to segment their entire global client base.
+
+  We brought the model to conduct localized data workshops with senior leadership in every region in which client does business. Using the outputs of the model and workshops, we formed a comprehensive set of strategic guidelines to optimize the way in which client goes to market with their products and services around the world.",
+  company: "IBM Global Business Services",
+  start_year: 2015,
+  end_year: 2015,
+)
+
+Project.create(
+  title: "Next generation demand forecasting",
+  client_name: "Major multinational retailer",
+  blurb: "Proof-of-concept development of a machine learning-based demand forecasting application for retail transformation",
+  description: "Client\'s supply chain organization was building a proof-of-concept application leveraging machine learning to enhance their demand forecasting operations. The goal was to be able to accurately predict demand for products at each of their retail locations using a combination of unstructured and structured data sources such as social media feeds, weather forecasts, market disruptions, and historical trends.
+ 
+  We supported the data aggregation, preparation, and analysis of the machine learning model that powered the application. Work resulted in successful proof-of-concept for a select retail location, with improved demand forecasts from model outputs compared to baseline forecasting practices.",
+  company: "IBM Global Business Services",
+  start_year: 2014,
+  end_year: 2014,
+)
+
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
