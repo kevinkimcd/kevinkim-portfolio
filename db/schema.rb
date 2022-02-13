cdc03ca43cc6889593a6892531909ece1b374b9a
kevinkimcd <kevinkimcd@gmail.com>
2022-02-13 12:43:53 -0500

    created migration to add project ID to slides for association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_11_225047) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_13_174300) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.string "client_name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_11_225047) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "project_id"
   end
 
 end
