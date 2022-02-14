ba86fd761e381ac7e60661ac508751f19bb90bce
kevinkimcd <kevinkimcd@gmail.com>
2022-02-14 18:56:42 -0500

    corrected migration error and schema

--- db/migrate/20220213174300_add_project_id_to_slides.rb
+++
@@ -1,5 +0,0 @@
-class AddProjectIdToSlides < ActiveRecord::Migration[7.0]
-  def change
-    add_column :slides, :project_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_13_174300) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_11_225047) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.string "client_name"
@@ -26,9 +26,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_13_174300) do
   create_table "slides", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.integer "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "project_id"
+    t.index ["project_id"], name: "index_slides_on_project_id"
   end
 
+  add_foreign_key "slides", "projects"
 end
