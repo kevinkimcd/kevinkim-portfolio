1c4f2b62a21f77931907a769e7a12eab6726770b
kevinkimcd <kevinkimcd@gmail.com>
2022-04-01 12:04:23 -0400

    attempted javascript for arrow in work page, but commented out for now

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "application"
+
+
+// const arrowTrigger = document.querySelector("#work-arrow-up");
+// let viewportHeight = window.innerHeight;
+
+// document.addEventListener("scroll", () => {
+//   if (window.scrollY >= arrowTrigger.getBoundingClientRect().bottom + viewportHeight) {
+//     console.log("Reached trigger point");
+//   }
+// })
