093aaafc091751cc5de4eaecb34e855e790b2d6b
kevinkimcd <kevinkimcd@gmail.com>
2022-04-02 18:27:00 -0400

    first attempt at dynamic arrow in work page, not working yet as expected

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,10 +6,32 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "application"
 
+let arrowDown = true;
+
+$(document).on("ready", function() {
+  $.fn.setArrow();
+})
+
 $(window).on('scroll', function() {
-  if ($(window).scrollTop() >= $(
-    '#work-subtitle').offset().top + $('#work-subtitle').
-      outerHeight() - window.innerHeight) {
-      console.log("Hello world");
-  }
+  $.fn.setArrow();
 });
+
+$.fn.setArrow = function () {
+  let arrowUpPosition = Math.floor($("#work").offset().top);
+  if ($(window).scrollTop() >= arrowUpPosition) {
+    if (arrowDown == true) {
+      $("#work-arrow-down").removeClass("down-arrow-on");
+      $("#work-arrow-up").addClass("arrow-up-on");
+      arrowDown = false;
+    }
+  } else if ($(window).scrollTop() < arrowUpPosition) {
+    if (arrowDown == false) {
+      $("work-arrow-up").removeClass("arrow-up-on");
+      $("work-arrow-down").addClass("arrow-down-on");
+      arrowDown = true;
+    }
+  }
+  console.log("scrollTop = " + $(window).scrollTop());
+  console.log("arrowUpPosition = " + arrowUpPosition);
+  console.log(arrowDown);
+}
