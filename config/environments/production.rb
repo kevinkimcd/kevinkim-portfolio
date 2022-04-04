383fe1deedf8ed704132d28c06c785a3f8b3b987
kevinkimcd <kevinkimcd@gmail.com>
2022-04-04 17:26:20 -0400

    added config.static_cache_control to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # added to quicken the speed with which the banner images load
+  config.static_cache_control = "public, max-age=31536000"
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
