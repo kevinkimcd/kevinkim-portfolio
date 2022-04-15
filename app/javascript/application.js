725c0a84cfac5702aac09092d3c104a930e346a7
kevinkimcd <kevinkimcd@gmail.com>
2022-04-15 08:16:23 -0400

    updated javascript to only load on specified pages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,45 +10,57 @@ import "application"
 // scripts for arrow in work index page
 
 
-if ($(".projects-index-view").length) {
-  $(window).on('scroll', function() {
-    $.fn.checkScrollPosition();
-  });
+$(window).on('scroll', function() {
+  $.fn.checkScrollPosition();
+});
 
-  $(function() {
-    $.fn.checkScrollPosition();
-  });
+$(function() {
+  $.fn.checkScrollPosition();
+});
 
-  $.fn.setArrowDown = function() {
+$.fn.setArrowDown = function() {
+  if ($("body").hasClass("projects-index-view")) {
     $("#work-arrow-down").addClass("arrow-activate");
   }
-  
-  $.fn.setArrowUp = function() {
+}
+
+$.fn.setArrowUp = function() {
+  if ($("body").hasClass("projects-index-view")) {
     $("#work-arrow-up").addClass("arrow-activate");
   }
-  
-  $.fn.removeArrowDown = function() {
+}
+
+$.fn.removeArrowDown = function() {
+  if ($("body").hasClass("projects-index-view")) {
     $("#work-arrow-down").removeClass("arrow-activate");
   }
-  
-  $.fn.removeArrowUp = function() {
+}
+
+$.fn.removeArrowUp = function() {
+  if ($("body").hasClass("projects-index-view")) {
     $("#work-arrow-up").removeClass("arrow-activate");
   }
-  
-  $(".work-arrow").first().on("click", function() {
+}
+
+$(".work-arrow").first().on("click", function() {
+  if ($("body").hasClass("projects-index-view")) {
     $("html, body").animate({
       scrollTop: $("#work-arrow-up").offset().top - Math.floor($("#work").height())
     }, 500);
-  });
-  
-  $(".work-arrow").last().on("click", function() {
+  }
+});
+
+$(".work-arrow").last().on("click", function() {
+  if ($("body").hasClass("projects-index-view")) {
     $("html, body").animate({
       scrollTop: $("#work-top").offset().top
     }, 500);
-  });
-  
-  
-  $.fn.checkScrollPosition = function() {
+  }
+});
+
+
+$.fn.checkScrollPosition = function() {
+  if ($("body").hasClass("projects-index-view")) {
     let arrowUpBreakPoint = Math.floor($("#work-arrow-up").offset().top) - Math.floor($("#work").height() * 2);
     let arrowDownBreakPoint = Math.floor($("#work-arrow-up").offset().top) - Math.floor($("#work").height());
     let scrollBottom = $(window).scrollTop() + $(window).height();
