60f723872cc028bb1f5cc4d756896f4190fed331
Kevin Kim <github@kevinckr.com>
2023-09-25 19:23:43 -0400

    updated CV filename

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@ class PagesController < ApplicationController
   end
 
   def download_resume
-    send_file "#{Rails.root}/app/assets/docs/resume.pdf", type: "application/pdf", x_sendfile: true
+    send_file "#{Rails.root}/app/assets/docs/kevinkim_cv.pdf", type: "application/pdf", x_sendfile: true
   end
 end
